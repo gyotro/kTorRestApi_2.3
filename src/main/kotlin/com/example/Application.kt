b6d6ad69017bdfb8eedd58c88bd1e2286d6bfe8c
gyotro <giovanni.dintrono@gmail.com>
2023-04-27 17:17:48 +0200

    first commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import io.ktor.server.application.*
+import com.example.plugins.*
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.Level
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
+    }
+    configureSerialization()
+    configureRouting()
+}
